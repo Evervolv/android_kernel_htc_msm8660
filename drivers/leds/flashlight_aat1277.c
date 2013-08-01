77aa6170f2b472c29ade41b0b8626da6afba7d74
Nick Reuter <nreuter85@gmail.com>
2013-07-31 20:39:35 -0500

    aat1277: fix includes
    Change-Id: I55686c9b0bc04365578f9c8ed2b0127f925e4ae1

--- drivers/leds/flashlight_aat1277.c
+++ drivers/leds/flashlight_aat1277.c
@@ -19,6 +19,7 @@
 
 
 #include <asm/gpio.h>
+
 #include <linux/earlysuspend.h>
 #include <linux/platform_device.h>
 #include <linux/htc_flashlight.h>
@@ -27,6 +28,7 @@
 #include <linux/leds.h>
 #include <linux/hrtimer.h>
 #include <linux/slab.h>
+#include <linux/module.h>
 
 #define FLT_DBG_LOG(fmt, ...) \
 		printk(KERN_DEBUG "[FLT]" fmt, ##__VA_ARGS__)
